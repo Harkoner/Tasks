c4a8c57daad93977236b52c1601e3de9397d78d3
NikolayKriger <nemec.ru.1996@gmail.com>
2022-10-22 19:57:21 +0700

    Добавлены ограничения по вводу, изменены циклы функций taskn(), введены области значения для класса Task5

--- Tasks/FirstTask.py
+++ Tasks/FirstTask.py
@@ -1,4 +1,18 @@
-from array import *
+def taskn():
+    tasks = (Task1(), Task2(), Task3(), Task4(), Task5())
+    print('For now, is ready for check 5 tasks'.center(50))
+    while True:
+            tasknumber = input('Please choose the number of task you are looking for:'.center(50))
+            if tasknumber.isdigit():
+                tasknumber = int(tasknumber)
+                if tasknumber <= 5:
+                    tasks[tasknumber - 1].start()
+                else:
+                    print('Choose the correct number'.center(50))
+            elif tasknumber == 'esc':
+                break
+            else:
+                print('Choose the correct number'.center(50))
 #Task1
 class Task1:
     def start(self):
@@ -38,16 +52,22 @@ class Task2:
         print('Do you want to see a picture?')
         print('y/n')
         answer = input()
-        if answer == 'y':
-            self.picture()
-        else:
-            print('All right, go next')
+        while answer == 'y' or 'n':
+            if answer == 'y':
+                self.picture()
+                break
+            elif answer == 'n':
+                print('All right, go next')
+                break
+            elif answer != 'y' or 'n':
+                Task2.start(self)
+                break
+
 
 """
 На самом деле не разобрался как задать область значений для answer1, чтобы он принимал только два значения
 Чтобы вызвать проверку между y/n, но в остальном работает. 
-Так же не совсем научился работать с самим "рисованием", выглядит как магия, даже если понимать как это
-работает
+Так же не совсем научился работать с самим "рисованием", выглядит как магия, даже если понимать как это работает
 """
 
 #Task3
@@ -55,8 +75,8 @@ class Task3:
     def start(self):
         print('Task#3')
         input('Нажмите любую клавишу чтобы увидеть ответ')
-        firstname = 'Ivan'
-        secondname = 'Ivanovich'
+        firstname = 'ivan'
+        secondname = 'ivanovich'
         print('Hello', firstname.title(), secondname.title()+'!', 'You just delved into Python. Great start!')
         input('Нажмите любую клавишу чтобы продолжить')
 
@@ -70,52 +90,40 @@ class Task4:
         print('Do you wanna see the magic?')
         print('y/n')
         answer = input()
-        if answer == 'y':
-              print(f"Lets format our numer into the money equivalent \nNew format: {'{0:3,.3f}'.format(amount)}")
-        else:
-              print('All right, go next for the Task#5')
-
+        while answer == 'y' or 'n':
+            if answer == 'y':
+                print(f"Lets format our numer into the money equivalent \nNew format: {amount} to {'{0:3,.3f}'.format(amount)}")
+                break
+            elif answer == 'n':
+                print('All right, go next')
+                break
+            elif answer != 'y' or 'n':
+                Task4.start(self)
+                break
 #Task5
 class Task5:
     def start(self):
         """К сожалению я не совсем научился именно рисовать эти ковры, но попытался реализовать работу оператора"""
-#12312312
         print('Task#5')
         print('Welcome to the carpet calculator.')
-        height_number = input("Please enter the height between 10 and 99: ")
-        height = int(height_number)
-        if height >= 10:
-            width = height * 3
-            print(f"Your carpet settings is {height:} and {width:}\n")
-            print('Lets make them!'.center(width))
-            for stick_count in range(1, height, 2):
-                print(('.|.' * stick_count).center(width, '-'))
-            print('Presented by Nikolay Kriger'.center(width, '-'))
-            for stick_count in range(height-2, 0, -2):
-                print(('.|.' * stick_count).center(width, '-'))
-        else:
-            height_number = input("Please try again and enter the height between 10 and 99: ")
-            height = int(height_number)
-            if height >= 10:
-                width = height * 3
-                print(f"Your carpet settings is {height:} and {width:}\n")
-                print('Lets make them!'.center(width))
-                for stick_count in range(1, height, 2):
-                    print(('.|.' * stick_count).center(width, '-'))
-                print('Presented by Nikolay Kriger'.center(width, '-'))
-                for stick_count in range(height - 2, 0, -2):
-                    print(('.|.' * stick_count).center(width, '-'))
+        while True:
+            height_number = input("Please enter the height between 10 and 99: ")
+            if height_number.isdigit():
+                height = int(height_number)
+                if height in range(10, 100, 1):
+                    width = height * 3
+                    print(f"Your carpet settings is {height:} and {width:}\n")
+                    print('Lets make them!'.center(width))
+                    for stick_count in range(1, height, 2):
+                        print(('.|.' * stick_count).center(width, '-'))
+                    print('Presented by Nikolay Kriger'.center(width, '-'))
+                    for stick_count in range(height - 2, 0, -2):
+                        print(('.|.' * stick_count).center(width, '-'))
+                    break
+                elif height not in range(10, 100, 1):
+                    print('Choose the correct number'.center(50))
             else:
-                print('We are sorry but we cannot make this size of carpet.')
-        print('Thank you for using our service!')
-
-
-def taskn():
-    tasks = (Task1(), Task2(), Task3(), Task4(), Task5())
-    print('For now, is ready for check 5 tasks'.center(50))
-    while True:
-        tasknumber = int(input('Please choose the number of task you are looking for:'.center(50)))
-        tasks[tasknumber-1].start()
-#asdasdasdasd
+                print('Choose the correct number1'.center(50))
+        return
 if __name__ == '__main__':
     taskn()
