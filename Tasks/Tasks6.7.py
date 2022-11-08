23f80b96f6fe2fa061cf29a238fd79704d37af01
NikolayKriger <nemec.ru.1996@gmail.com>
2022-11-08 23:20:13 +0700

    Решены последние задания. Написаны тесты для каждого задания

--- Tasks/Tasks6.7.py
+++ Tasks/Tasks6.7.py
@@ -1,19 +1,50 @@
 from random import randint
 import random
+import string
+import requests
 
 
 def tests(num_task: 'Contains a number of func we want to test',
-          count: 'contains a tech param') -> 'Call a func: tasks[num_task](test object, count)':
+          count: 'contains a tech param',
+          num_test: 'contains a number of tests',
+          WORDS: 'type = list') -> 'Call a func: tasks[num_task](test object, count)':
+    tasks = {
+        1: task1,
+        2: task2,
+        3: task3,
+        4: task4,
+        5: task5,
+    }
     if num_task == 1:
-        return tasks[num_task](randint(5, 15), count)
+        test_list = []
+        for x in range(5):
+            test_list.append(randint(0, 20))
+        return tasks[num_task](test_list, count)
     if num_task == 2:
-        return tasks[num_task](randint(5, 15), count)
+        test_list = []
+        for x in range(5):
+            test_list.append(random.uniform(-5.0, 15.0))
+        return tasks[num_task](test_list, count)
     if num_task == 3:
         test_list = []
         for x in range(5):
             test_list.append(randint(-20, 70))
         return tasks[num_task](test_list, count)
-
+    if num_task == 4:
+        test_list = []
+        for x in range(randint(1, 7)):
+            test_list.append(randint(0, 200))
+        return tasks[num_task](test_list, count)
+    if num_task == 5:
+        list_of_words = WORDS[:]
+        sent = []
+        text = ''
+        for i in range(randint(3, 7)):
+            sent.append(str(random.choice(list_of_words))[2:-1])
+            word = ''
+        for x in sent:
+            text += x + ' '
+        return tasks[num_task](text[:-1], count)
 
 def caller(num):
     if num == 1:
@@ -22,10 +53,15 @@ def caller(num):
         return 'Task#2 [-Junior]'
     if num == 3:
         return 'Task#3 [-Junior]'
+    if num == 4:
+        return 'Task#4 [Junior]'
+    if num == 5:
+        return 'Task#5 [+Junior]'
 
 
 def start() -> 'Starting func':
     i = 0
+    WORDS = []
     num = input('please type the number of task: ')
     if num != 'exit':
         num = int(num)
@@ -33,15 +69,19 @@ def start() -> 'Starting func':
         num_test = 1
         if test == 'y':
             num_test = int(input('Type the number of tests you would like to see: '))
+        if num == 5:
+            word_site = "https://www.mit.edu/~ecprice/wordlist.10000"
+            response = requests.get(word_site)
+            WORDS = response.content.splitlines()
         print(caller(num))
         while i < num_test:
-            print(tests(num, i+1))
+            print(tests(num, i+1, num_test, WORDS))
             i += 1
         start()
 
 
 # task1
-def task1(num: 'testing obj type = int, in range[1:n]',
+def task1(raw_list: 'Type = list',
           count: 'tech param') -> 'Started a task1 with num which is param of testing obj':
     """
     Дано: список (list) целых чисел (int).
@@ -51,19 +91,9 @@ def task1(num: 'testing obj type = int, in range[1:n]',
     elements = [1, 3, 5], результат: 30
     elements = [] , результат: 0
     """
-    #TODO: Переместить лист в аргумент функции, и сделать генерацию в функции тест. Сократить функцию.
-    elements = []
-    i = randint(0, int(num))
-    tmp = i
-    if i != 0:
-        while i > 0:
-            elements.append(randint(0, 9))
-            i -= 1
-        result = sum(elements[::2])
-    else:
-        result = 0
-    result = result * tmp
-    return f'{count}) Generated list: {elements}; {result = }'
+    elements = raw_list[:]
+    tmp = elements[-1]
+    return f'{count}) Generated list: {raw_list}; result: list{elements[::2]}, Sum = {sum(elements[::2])}, devided by last number = {sum(elements[::2])*tmp if elements != [] else 0}'
 
 
 def cutter(list: 'type = list, contains an items with non sorted digits format',
@@ -82,7 +112,7 @@ def cutter(list: 'type = list, contains an items with non sorted digits format',
 
 
 # Task2
-def task2(num, count):
+def task2(raw_list, count):
     """
     Дано: массив чисел (float или/и int).
     Задание: нужно найти разницу между самым большим (максимум) и самым малым (минимум) элементом. Если список пуст, то результат равен 0 (ноль).
@@ -93,19 +123,12 @@ def task2(num, count):
     elements = [10.2, -2.2, 0, 1.1, 0.5], результат: 12.4
     elements = [] , результат: 0
     """
-    #TODO: Переместить лист в аргумент функции, и сделать генерацию в функции тест. Сократить функцию.
-    elements = []
-    i = randint(0, int(num))
-    if i != 0:
-        while i > 0:
-            elements.append(random.uniform(0, 15.0))
-            i -= 1
+    elements = raw_list[:]
+    if elements != []:
         result = max(elements) - min(elements)
-        if result < 0:
-            result = result * -1
     else:
         result = 0
-    return f'{count}) Elements: ' + str(cutter(elements, 4)) + ' result: ' + '{0:.3f}'.format(result)
+    return f'{count}) Elements: ' + str(cutter(elements, 4)) + ' result: ' + '{0:.3f}'.format(abs(result))
 
 
 # Task3
@@ -121,29 +144,72 @@ def task3(elements, count):
     elements = (-1, -2, -3, 0), результат: [0, -1, -2, -3]
     """
     temp_elements = elements[:]
-    for x in temp_elements:
-        if x < 0:
-            temp_elements[temp_elements.index(x)] = x * -1
-    temp_elements.sort()
-    for y in elements:
-        if y < 0:
-            temp_elements[temp_elements.index(y * -1)] = temp_elements[temp_elements.index(y * -1)] * -1
+    temp_elements.sort(key=abs)
     return f'{count}) ' + 'reference: {}, result: {}'.format(elements, temp_elements)
 
 
-tasks = {
-        1: task1,
-        2: task2,
-        3: task3
-        # '4': task4(),
-        # '5': task5(),
-        # '6': task6()
-    }
-# tests = {
-#         1: '5',
-#         2: '5'
-#     }
+def task4(raw_list, count):
+    """
+    Дано: кортеж или список чисел.
+    Задание: Медиана - это числовое значение, которое делит сортированый массив чисел на большую и меньшую половины.
+    В сортированом массиве с нечетным числом элементов медиана - это число в середине массива.
+    Для массива с четным числом элементов, где нет одного элемента точно посередине,
+    медиана - это среднее значение двух чисел, находящихся в середине массива.
+    В этой задаче дан непустой массив натуральных чисел.
+    Вам необходимо найти медиану данного массива.
+    Пример:
+    elements = [1, 2, 3, 4, 5], результат: 3
+    elements = [3, 1, 2, 5, 3], результат: 3
+    elements = [1, 300, 2, 200, 1], результат: 2
+    elements = [3, 6, 20, 99, 10, 15], результат: 12.5
+    """
+    temp_elements = raw_list[:]
+    temp_elements.sort()
+    index_temp_elements = int(len(temp_elements) / 2)
+    if len(temp_elements) % 2 == 0:
+        return f'{count}) referenced: {raw_list} sorted: {temp_elements} result: {(temp_elements[index_temp_elements] + temp_elements[index_temp_elements - 1]) / 2}'
+    else:
+        return f'{count}) referenced: {raw_list} sorted: {temp_elements} result: {(temp_elements[index_temp_elements])}'
 
+
+def task5(text, count):
+    """
+    Дано: текст, как строка (str).
+    Задание: Наши Роботы никогда не упускают возможности, чтобы улучшить свои навыки в лингвистике. Сейчас они изучают английский алфавит и что с этим делать.
+    Алфавит разделен на гласные и согласные буквы (да, мы разделили буквы, а не звуки).
+    Гласные -- A E I O U Y
+    Согласные -- B C D F G H J K L M N P Q R S T V W X Z
+    Дан текст с разными словами и/или числами, которые разделены пробелами и знаками пунктуации.
+    Числа не считаются за слова (также как и смесь букв и цифр).
+    Необходимо подсчитать слова, в которых гласные буквы чередуются с согласными (полосатые слова),
+    то есть в таких словах нет двух гласных или двух согласных букв подряд.
+    Слова состоящие из одной буквы - не "полосатые" (не считайте их).
+    Регистр букв не имеет значения.
+    Пример:
+    text = "My name is ...", результат: 3
+    text = "Hello world", результат: 0
+    text = "A quantity of striped words.", результат: 1
+    text = "Dog,cat,mouse,bird.Human.", результат: 3
+    """
+    vowels = 'aAeEiIoOuUyY'
+    consonant = 'bBcCdDfFgGhHjJkKlLmMnNpPqQrRsStTvVwWxXzZ'
+    i = 0 #num of liter
+    answer = 0
+    final_answer = 0
+    text_list = text.split()
+    for words in text_list:
+        if len(words) != 1:
+            while i + 1 < len(words):
+                if words[i] in vowels and words[i+1] in vowels:
+                    answer += 1
+                elif words[i] in consonant and words[i + 1] in consonant:
+                    answer += 1
+                i += 1
+            i = 0
+            if answer == 0:
+                final_answer += 1
+            answer = 0
+    return f'{count}) referenced: "{text}" result: {final_answer}'
 
 if __name__ == '__main__':
     start()
